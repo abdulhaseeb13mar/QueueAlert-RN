22b20d538925746d452abae6fff9300318b8d01a
abdulhaseeb13mar <abdulhaseeb13mar@gmail.com>
2021-06-04 16:25:23 +0500

    most basic prototype of vendor and people ready with queue listener

--- src/screens/people/homeScreen/index.js
+++ src/screens/people/homeScreen/index.js
@@ -1,13 +1,28 @@
-import React from 'react';
+/* eslint-disable react-native/no-inline-styles */
+import React, {useEffect, useState} from 'react';
 import {View, Text} from 'react-native';
-
-//Styling
 import styles from './style';
+import firestore from '@react-native-firebase/firestore';
 
 const HomePage = () => {
+  const [currentNumber, setCurrentNumber] = useState(0);
+  useEffect(() => {
+    const subscriber = firestore()
+      .collection('Queues')
+      .doc('ka8ivUqheDdJ1MEWvuPTKMIw8BQ2')
+      .onSnapshot(documentSnapshot => {
+        console.log('Current Number: ', documentSnapshot.data().currentNum);
+        setCurrentNumber(documentSnapshot.data().currentNum);
+      });
+    return () => subscriber();
+  }, []);
   return (
     <View style={styles.container}>
       <Text>People HomePage Screen</Text>
+      <Text>ABDUL HASEEB QUEUE ONGOING...</Text>
+      <Text style={{fontSize: 100}}>
+        {currentNumber < 10 ? `0${currentNumber}` : currentNumber}
+      </Text>
     </View>
   );
 };
